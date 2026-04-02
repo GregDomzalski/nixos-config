7bbb77b5023e55c0bc867e0434443583cf92a256
Greg Domzalski <greg@gregdom.com>
2026-04-02 17:22:13 -0400

    mkCert tool for development

---
+++ profiles/development/nixos/local-certs.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.profiles.development.localCerts;
+in {
+  options.profiles.development.localCerts = {
+    enable = mkOption {
+      type = types.bool;
+      default = config.profiles.development.enable;
+      description = ''
+        Enable local development certificate support via mkcert.
+
+        After enabling and rebuilding, run `mkcert -install` to:
+        - Create a local CA certificate at ~/.local/share/mkcert/rootCA.pem
+        - Install the CA to ~/.pki/nssdb (Chromium's trust store)
+        - Make all mkcert-generated certificates trusted by browsers
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Install mkcert and NSS tools system-wide
+    environment.systemPackages = with pkgs; [
+      mkcert  # Tool for creating locally-trusted development certificates
+      nss     # Provides certutil for managing Chromium's certificate database
+    ];
+
+    # Add mkcert CA to system PKI for OpenSSL-based tools (curl, git, etc.)
+    # Note: This path won't exist until user runs `mkcert -install`
+    security.pki.certificateFiles = mkIf (pathExists "/home/greg/.local/share/mkcert/rootCA.pem") [
+      "/home/greg/.local/share/mkcert/rootCA.pem"
+    ];
+  };
+}
